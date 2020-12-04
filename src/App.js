f184b17483afdf19e7c0065ab93ddd6717061bed
Greg <g.rutherford10@gmail.com>
2020-12-04 11:16:19 +0000

    add functionality to create single question & add to question state

--- src/App.js
+++ src/App.js
@@ -10,16 +10,38 @@ class App extends Component {
 			countries: [],
 			questions: [],
 		};
+		this.createQuestions = this.createQuestions.bind(this);
 	}
 
-	componentDidMount() {
-		axios("https://restcountries.eu/rest/v2/all")
+	async componentDidMount() {
+		await axios("https://restcountries.eu/rest/v2/all")
 			.then((res) =>
 				this.setState({
 					countries: res.data,
 				})
 			)
 			.catch((err) => console.error(err));
+
+		await this.createQuestions();
+	}
+
+	createQuestions() {
+		const arrayLength = this.state.countries.length;
+		const random = Math.floor(Math.random() * arrayLength);
+		const country = this.state.countries[random].name;
+		const capital = this.state.countries[random].capital;
+		const question = {
+			question: `${capital} is the capital of what country?`,
+			answers: [
+				country,
+				this.state.countries[Math.floor(Math.random() * arrayLength)].name,
+				this.state.countries[Math.floor(Math.random() * arrayLength)].name,
+				this.state.countries[Math.floor(Math.random() * arrayLength)].name,
+			],
+		};
+		this.setState({
+			questions: this.state.questions.concat(question),
+		});
 	}
 
 	render() {
@@ -27,7 +49,7 @@ class App extends Component {
 			<div className="app-container">
 				<div className="question-title">
 					<h2>Countries Quiz</h2>
-					<img src={QuestionSVG} />
+					<img src={QuestionSVG} alt="adventure svg" />
 				</div>
 				<div className="question-container">
 					<h4>Question</h4>
